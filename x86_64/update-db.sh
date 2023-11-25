5ff8c75cf31133aeb2fdcd0d694fe11f8d0239c5
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2023-11-25 22:51:45 +0530

    +upd

---
+++ x86_64/update-db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm eshanizedos-repository-bloodline-bloodline*
+
+echo "repo-add"
+repo-add -s -n -R eshanizedos-repository-bloodline.db.tar.gz *.pkg.tar.zst
+
+sleep 1
+
+rm eshanizedos-repository-bloodline.db
+rm eshanizedos-repository-bloodline.db.sig
+
+rm eshanizedos-repository-bloodline.files
+rm eshanizedos-repository-bloodline.files.sig
+
+mv eshanizedos-repository-bloodline.db.tar.gz eshanizedos-repository-bloodline.db
+mv eshanizedos-repository-bloodline.db.tar.gz.sig eshanizedos-repository-bloodline.db.sig
+
+mv eshanizedos-repository-bloodline.files.tar.gz eshanizedos-repository-bloodline.files
+mv eshanizedos-repository-bloodline.files.tar.gz.sig eshanizedos-repository-bloodline.files.sig
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
